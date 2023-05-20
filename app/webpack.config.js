c6071d4c8a5742b161e7e1c8f9f749a518f49e33
Jan Czechowski <przemyslaw.czechowski@gmail.com>
2023-05-20 12:19:10 +0200

    fix webpack

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,7 +2,9 @@ const path = require('path');
 
 module.exports = {
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
+    static: {
+      directory: path.join(__dirname, "public/")
+    },
     compress: false,
     host: '0.0.0.0',
     port: 8080,
